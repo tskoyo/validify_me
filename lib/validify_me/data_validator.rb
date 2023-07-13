32952e2236eb32a83d5dede75a6c21878a042751
Jovan <jovansremacki@gmail.com>
2023-07-13 22:31:08 +0200

    DataValidator improvements

--- lib/validify_me/data_validator.rb
+++ lib/validify_me/data_validator.rb
@@ -9,7 +9,6 @@ module ValidifyMe
   module DataValidator
     def self.included(base)
       base.extend(ClassMethods)
-      # base.include(InstanceMethods)
     end
 
     # This module defines methods that will be called on class-level
@@ -28,19 +27,15 @@ module ValidifyMe
         validator.params.each do |param|
           next if param.optional? && !params.key?(param.name) && param.data[:constraints].empty?
 
-          handle_param_types(param, params[param.name])
+          handle_param_type(param, params[param.name])
         end
       end
 
       private
 
-      def handle_param_types(param, param_value)
-        case param.data[:type]
-        when :integer
-          ValidifyMe::Validator::IntegerValidator.new(param, param_value).validate
-        when :string
-          ValidifyMe::Validator::StringValidator.new(param, param_value).validate
-        end
+      def handle_param_type(param, param_value)
+        validator_class_name = "ValidifyMe::Validator::#{param.data[:type].to_s.capitalize}Validator"
+        Object.const_get(validator_class_name).new(param, param_value).validate
       end
     end
 
@@ -76,8 +71,8 @@ module ValidifyMe
       end
 
       def value(type, **options)
-        data[:type] = type
-        data.merge!(constraints: options)
+        @data[:type] = type
+        @data.merge!(constraints: options)
       end
 
       def required?
